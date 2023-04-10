ad529b3645a66848c694bceda3d0fc37c3f350c5
faisalalm01 <faisalali2858@gmail.com>
2023-04-10 21:37:10 +0700

    add .env.example

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config({})
 const express = require('express');
 const app = express();
-const port = 3002;
+const cors = require('cors')
+const port = process.env.PORT;
 const mainRoutes = require('./src/routes')
 
+app.use(cors())
 app.use(express.urlencoded({extended: false}))
-
 app.use('/', mainRoutes)
+
+
 app.listen(port, () => {
-    console.log(`Server run on port:${port}`);
+    console.log(`Server run on port: http://localhost:${port}`);
 })
